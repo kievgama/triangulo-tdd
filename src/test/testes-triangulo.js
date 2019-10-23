8ddec6fa0d7f96fcc46ed5f925bfe3653a658a95
kievgama <kiev.gama@gmail.com>
2019-10-23 07:11:41 -0300

    Removendo console.log

--- src/test/testes-triangulo.js
+++ src/test/testes-triangulo.js
@@ -4,11 +4,9 @@ const Triangulo = require('../triangulo');
 const assert = chai.assert;
 
 
-
 describe('Verificando se é escaleno', () => {
   it('Teste: Os três lados são diferentes', () => {
     let triangulo = new Triangulo(7, 5, 10)
-    console.log(triangulo.toString());
     assert.equal(triangulo.tipo(), "Escaleno");
   });
 
