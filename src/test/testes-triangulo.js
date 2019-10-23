89be34dd8c583724ec1efd05f12772fc3f55adec
kievgama <kiev.gama@gmail.com>
2019-10-23 06:18:17 -0300

    Initial commit

---
+++ src/test/testes-triangulo.js
@@ -0,0 +1,44 @@
+const chai = require('chai');
+const Triangulo = require('../triangulo');
+
+const assert = chai.assert;
+
+
+
+describe('Verificando se é escaleno', () => {
+  it('Teste: Os três lados são diferentes', () => {
+    let triangulo = new Triangulo(7, 5, 10)
+    console.log(triangulo.toString());
+    assert.equal(triangulo.tipo(), "Escaleno");
+  });
+
+});
+
+describe('Verificando se é equilatero', () => {
+  it('Teste: Os três lados são iguais', () => {
+    let triangulo = new Triangulo(5, 5, 5)
+    assert.equal(triangulo.tipo(), "Equilatero");
+  });
+});
+
+describe('Verificando se é isósceles', () => {
+  it('Teste: Lados a e b iguais', () => {
+    let triangulo = new Triangulo(7, 7, 10)
+    assert.equal(triangulo.tipo(), "Isosceles");
+  });
+  it('Teste: Lados b e c iguais', () => {
+    let triangulo = new Triangulo(10, 7, 7)
+    assert.equal(triangulo.tipo(), "Isosceles");
+  });
+  it('Teste: Lados a e c iguais', () => {
+    let triangulo = new Triangulo(7, 10, 7)
+    assert.equal(triangulo.tipo(), "Isosceles");
+  });
+});
+
+describe('Verificando se os tamanhos lados são válidos', () => {
+  it('Teste: Lado a ', () => {
+    let triangulo = new Triangulo(100, 10, 10)
+    assert.equal(triangulo.valido(), false);
+  });
+});
---
+++ src/triangulo.js
@@ -0,0 +1,13 @@
+function Triangulo(a,b,c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  
+  Triangulo.prototype.toString = function () {
+    return `Lado A ${this.a}, Lado B ${this.b}, Lado C ${this.c}`
+  };
+  
+  
+  module.exports = Triangulo;
