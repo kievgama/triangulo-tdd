ff906bc74c0868f9ece788a8a89824d75db35ba9
kievgama <kiev.gama@gmail.com>
2019-11-13 07:01:59 -0300

    Update

--- src/triangulo.js
+++ src/triangulo.js
@@ -2,9 +2,18 @@ function Triangulo(a,b,c) {
     this.a = a;
     this.b = b;
     this.c = c;
+    
   }
 
-  
+  Triangulo.prototype.tipo = function(){
+    if((this.a != this.b) && (this.b != this.c) && (this.a != this.c)){
+      return 'Escaleno'
+    }else if((this.a === this.b) && (this.b === this.c)){
+      return 'Equilatero'
+    }else{
+      return 'Isosceles'
+    }
+  }
   Triangulo.prototype.toString = function () {
     return `Lado A: ${this.a}, Lado B: ${this.b}, Lado C: ${this.c}`
   };
